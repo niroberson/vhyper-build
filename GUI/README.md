25fda2e9b52cf191a0c768525287739cdca2a1ff
sfbahr <sfbahr@gmail.com>
2016-02-28 04:53:21 -0500

    Finish the TCP socket test GUI, only server side
    I imagine the computer w/ the GUI will be the server but it should be
    fairly simple to change. This implementation of the host freezes the GUI
    while waiting for the accept call to return, so making it asynchronous or
    putting it on a separate thread should be done later. I'm investigating
    the different methods of doing so and how to integrate it with Qt.
    
    However, for now this is a functional way to test if the sockets are
    communicating correctly.
    
    Also added some more info to the README.

--- GUI/README.md
+++ GUI/README.md
@@ -1,3 +1,9 @@
 Visual C++ Redistributable for Visual Studio 2015 is required to run this
-application. So, if you receive an error when trying to run the gui, try
-installing vc_redist.x64.exe and then run it again.
+application. So, if you receive the error "pyi_rth_qt5plugins returned -1"
+trying to run the gui, try installing vc_redist.x64.exe and then run it again.
+
+In order to generate a .py file from a .ui file, install PyQt5 and run:
+`python -m PyQt5.uic.pyuic input.ui > output.py`
+
+To create a single .exe file from all of the python + Qt madness, install PyInstaller and run:
+`pyinstaller -wF main.py`
---
+++ GUI/SocketTest/tcpclient.py
@@ -0,0 +1,39 @@
+# Echo client program
+import socket
+import sys
+
+
+def main():
+    host = 'localhost'
+    port = 50008
+    s = None
+    for res in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        af, socktype, proto, canonname, sa = res
+        print('Trying to connect to af="{}" socktype="{}" proto="{}" canonname="{}" sa="{}"'.format(
+                                                                                af, socktype, proto, canonname, sa))
+        try:
+            s = socket.socket(af, socktype, proto)
+        except OSError as msg:
+            s = None
+            continue
+        try:
+            s.connect(sa)
+        except OSError as msg:
+            s.close()
+            s = None
+            continue
+        break
+    if s is None:
+        print('could not open socket')
+        sys.exit(1)
+    print('Connected to {}'.format(s.getpeername()))
+    while True:
+        data = s.recv(1024)
+        if not data:
+            break
+        print('Received and sending back: "{}"'.format(data.decode()))
+        s.send(data)
+    s.close()
+
+if __name__ == '__main__':
+    main()
