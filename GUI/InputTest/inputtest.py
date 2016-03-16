28bf7e088083755fa6006c7256e061c8e9c886b3
sfbahr <sfbahr@gmail.com>
2016-03-16 17:20:41 -0400

    WIP change to asynchrnous server

---
+++ GUI/InputTest/inputtest.py
@@ -0,0 +1,78 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+import inputgui
+import tcpserver
+
+
+class InputTestGui(socketgui.Ui_MainWindow):
+    def __init__(self, MainWindow):
+        super(SocketTestGui, self).__init__()
+        self.setupUi(MainWindow)
+        self.plainTextEditFilter = TextEditReturnFilter(tcpSend, updateBrowser)
+        self.plainTextEdit.installEventFilter(self.plainTextEditFilter)
+        self.plainTextEdit.setFocus()
+
+        self.server = None
+        self.pushButton_2.clicked.connect(self.toggleServer)
+
+    def toggleServer(self):
+        _translate = QtCore.QCoreApplication.translate
+        if not self.server:
+            try:
+                port = int(self.lineEdit_2.text())
+            except ValueError:
+                return
+            self.server = tcpserver.TcpServer(port)
+            addr = self.server.accept()  # blocking
+            self.textBrowser.append('Connected to {}'.format(addr))
+            self.pushButton_2.setText(_translate("MainWindow", "Close"))
+        else:
+            self.server.close()
+            self.server = None
+            self.textBrowser.append('Closed open connection')
+            self.pushButton_2.setText(_translate("MainWindow", "Host"))
+
+    def tcpSend(self):
+        if self.server:
+            print('sending "{}"'.format(self.plainTextEdit.toPlainText()))
+            received = self.server.send(self.plainTextEdit.toPlainText())
+            self.textBrowser.append('{} {}'.format('Received', repr(received)))
+
+    def updateBrowser(self):
+        text = self.plainTextEdit.toPlainText()
+        if text:
+            self.textBrowser.append(self.plainTextEdit.toPlainText())
+            self.plainTextEdit.setPlainText('')
+
+
+class TextEditReturnFilter(QtCore.QObject):
+    textEditReturn = QtCore.pyqtSignal()
+
+    def __init__(self, *funcs):
+        super(TextEditReturnFilter, self).__init__()
+        for func in funcs:
+            self.textEditReturn.connect(func)
+        #self.textEditReturn.connect(gui.tcpSend)
+        #self.textEditReturn.connect(gui.updateBrowser)
+
+    def eventFilter(self, srcObject, event):
+        if event.type() == QtCore.QEvent.KeyPress and event.key() == QtCore.Qt.Key_Return:
+            self.textEditReturn.emit()
+            return True
+        return super(TextEditReturnFilter, self).eventFilter(srcObject, event)
+
+
+class MyQPlainTextEdit(QtWidgets.QPlainTextEdit):
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Return:
+            print('"{}" was entered'.format(self.toPlainText()))
+            self.setPlainText('')
+        else:
+            super(MyQPlainTextEdit, self).keyPressEvent(event)
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    testgui = QtWidgets.QMainWindow()
+    ui = InputTestGui(testgui)
+    testgui.show()
+    sys.exit(app.exec_())
---
+++ GUI/InputTest/tcpserver.py
@@ -0,0 +1,64 @@
+import socket
+import sys
+from PyQt5 import QtCore
+
+class TcpServer(QtCore.QThread):
+    HOST = None               # Symbolic name meaning all available interfaces
+    recieved = QtCore.pyqtSignal()
+
+    def __init__(self, port=50008):
+        super(TcpServer, self).__init__()
+        self.s = None
+        for res in socket.getaddrinfo(self.HOST, port, socket.AF_UNSPEC,
+                                      socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+            af, socktype, proto, canonname, sa = res
+            try:
+                self.s = socket.socket(af, socktype, proto)
+            except OSError as msg:
+                self.s = None
+                continue
+            try:
+                self.s.bind(sa)
+                self.s.listen(1)
+            except OSError as msg:
+                self.s.close()
+                self.s = None
+                continue
+            break
+        if self.s is None:
+            print('could not open socket')
+            return
+        print('Created server socket af="{}" socktype="{}" proto="{}" canonname="{}" sa="{}"'.format(
+                                                                                af, socktype, proto, canonname, sa))
+        self.conn = None
+        self.addr = None
+
+    def accept(self):
+        self.conn, self.addr = self.s.accept()
+        print('Connected by', self.addr)
+        return self.addr
+
+    def send(self, message):
+        if not message:
+            return
+        self.conn.sendall(message.encode())
+        data = self.conn.recv(1024)
+        print('Received', repr(data))
+        return data
+
+    def close(self):
+        self.conn.close()
+
+
+def main():
+    server = TcpServer()
+    server.accept()
+    while True:
+        message = input()
+        if not message:
+            break
+        server.send(message)
+    server.close()
+
+if __name__ == '__main__':
+    main()
